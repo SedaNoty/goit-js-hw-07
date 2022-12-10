65c0741f5eefe9167a029c00b5f2dc8579e2f99e
SedaNoty <sedanoty5@gmail.com>
2022-12-11 00:16:51 +0200

    goit-js-hw-07_02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryPictures = document.querySelector('.gallery')
+const pictures = []
+
+galleryItems.forEach(element => {
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+	galleryImage.className = 'gallery__image'
+	galleryImage.src = element.preview
+	galleryImage.setAttribute('title', element.description)
+	galleryImage.alt = element.description
+
+	galleryLink.append(galleryImage)
+	pictures.push(galleryLink)
+})
+galleryPictures.append(...pictures)
+
+new SimpleLightbox('.gallery a', {
+    captionsData: `alt`,
+	captionDelay: 250,
+});

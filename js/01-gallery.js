cf68974be28a811276a319a5b38427168e463bd6
SedaNoty <sedanoty5@gmail.com>
2022-12-11 00:01:06 +0200

    goit-js-hw-07_01-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryPictures = document.querySelector('.gallery')
+
+const pictures = []
+
+galleryItems.forEach(element => {
+	const galleryElements = document.createElement('div')
+	galleryElements.className = 'gallery__item'
+	const galleryLink = document.createElement('a')
+	galleryLink.className = 'gallery__link'
+	galleryLink.href = element.original
+	const galleryImage = document.createElement('img')
+    galleryImage.className = 'gallery__image'
+    galleryImage.src = element.preview;
+    galleryImage.setAttribute('data-source', element.original)
+    galleryImage.alt = element.description;
+
+	galleryElements.append(galleryLink)
+	galleryLink.append(galleryImage)
+	pictures.push(galleryElements)
+})
+
+galleryPictures.append(...pictures)
+
+galleryPictures.addEventListener('click', e => {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') {
+		return
+	}
+
+    const selectedImage = e.target.getAttribute('data-source')
+
+    const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`)
+
+    instance.show()
+    
+    galleryPictures.addEventListener('keydown', e => {
+		if (e.key === 'Escape') {
+			instance.close()
+		}
+	})
+})
